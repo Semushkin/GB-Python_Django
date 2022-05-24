0ca4aa25f69d4c87067413d64946eb168515ac1d
Semushkin_Anton <rkzton@yandex.ru>
2022-05-24 16:19:55 +0300

    update

--- mainapp/context_processors.py
+++
@@ -1,8 +0,0 @@
-from basket.models import Basket
-
-def basket(request):
-    basket_list = []
-    if request.user.is_authenticated:
-        basket_list = Basket.objects.filter(user=request.user)
-
-    return {'baskets': basket_list}
--- mainapp/management/commands/update_user.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from authapp.models import User, UserProfile
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        users = User.objects.all()
-        for user in users:
-            UserProfile.objects.create(user=user)
-
